e9b7b4c93bbe65e4fe9615360e840227bcde9669
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2021-06-17 18:55:30 +0800

    [ components ] navbar

--- src/App.vue
+++ src/App.vue
@@ -2,45 +2,22 @@
   <!-- Vuetify 有兩個主要組件，v-app 和 v-main -->
   <!-- v-app 是應用程式根節點，直接替換預設的 Vue 入口 <div id="app"> -->
   <!-- v-main 是替換 mainHTML 元素應用程式的根節點內容 -->
-  <!-- Vuetify 支持淺色 light 和深色 dark 變體。預設情況下，您的應用程式將使用 light 主題。但透過添加 dark，可以覆蓋掉它。 -->
-  <!-- 加上 app 的話可以固定（類似於 position:fixed） v-app-bar 在最上方 -->
-  <!-- https://vuetifyjs.com/en/api/v-app-bar/#props -->
   <v-app>
-    <v-app-bar app dark class="light-green darken-3">
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
+    <Navbar />
+    <v-main class="grey lighten-2">
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  name: "App",
+  components: { Navbar },
+  data() {
+    return;
+  },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <!-- Vuetify 支持淺色 light 和深色 dark 變體。預設情況下，應用程式將使用 light 主題。但透過添加 dark，可以覆蓋掉它。 -->
+    <!-- 加上 app 的話可以固定（類似於 position:fixed） v-app-bar 在最上方 -->
+    <!-- https://vuetifyjs.com/en/api/v-app-bar/#props -->
+    <!-- 預設有陰影，若想達致扁平化設計（沒有陰影）：可以加上 flat -->
+    <v-toolbar app flat dark class="light-green darken-3">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
