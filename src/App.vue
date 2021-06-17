0bc7837f37465949a6cd84aab1676df829edbf5b
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2021-06-17 17:49:38 +0800

    the start of router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- Vuetify 有兩個主要組件，v-app 和 v-main -->
+  <!-- v-app 是應用程式根節點，直接替換預設的 Vue 入口 <div id="app"> -->
+  <!-- v-main 是替換 mainHTML 元素應用程式的根節點內容 -->
+  <!-- Vuetify 支持淺色 light 和深色 dark 變體。預設情況下，您的應用程式將使用 light 主題。但透過添加 dark，可以覆蓋掉它。 -->
+  <!-- 加上 app 的話可以固定（類似於 position:fixed） v-app-bar 在最上方 -->
+  <!-- https://vuetifyjs.com/en/api/v-app-bar/#props -->
+  <v-app>
+    <v-app-bar app dark class="light-green darken-3">
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <h1>HOME PAGE</h1>
+  </div>
+</template>
