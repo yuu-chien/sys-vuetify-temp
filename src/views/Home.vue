20a371ae7dd9f2f469c3d86df92e6180283c8b10
chien.yu <sarsh40320@gmail.com>
2021-07-30 18:38:12 +0800

    [ Home ] mobile first

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,20 @@
 <template>
   <v-container>
-    <!-- <v-bottom-navigation
+    <v-bottom-navigation
+      absolute
+      shift
       color="orange"
-      horizontal
-      max-height="52"
-      class="mx-auto"
+      class="p-0"
+      height="auto"
     >
-      <v-btn x-large color="white" max-width="100%">
-        <span>我要打卡</span>
-        <v-icon>mdi-account-check</v-icon>
+      <v-btn v-for="(item, i) in items" :key="i" x-large color="white">
+        <span class="mt-1">{{ item.text }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
       </v-btn>
-      <v-btn x-large color="white" max-width="100%">
-        <span>打卡紀錄</span>
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-      <v-btn x-large color="white" max-width="100%">
-        <span>補登申請</span>
-        <v-icon>mdi-book-plus-multiple-outline</v-icon>
-      </v-btn>
-      <v-btn x-large color="white" max-width="100%">
-        <span>補登審核</span>
-        <v-icon>mdi-account-multiple-check-outline</v-icon>
-      </v-btn>
-      <v-btn x-large color="white" max-width="100%">
-        <span>人事查詢</span>
-        <v-icon>mdi-key-link</v-icon>
-      </v-btn>
-    </v-bottom-navigation> -->
-    <v-card max-width="360">
+    </v-bottom-navigation>
+    <router-view></router-view>
+    <!-- <v-card max-width="360">
       <v-list shaped>
-        <!-- <v-subheader>REPORTS</v-subheader> -->
         <v-list-item-group v-model="selectedItem" color="orange">
           <v-list-item v-for="(item, i) in items" :key="i">
             <v-list-item-icon>
@@ -41,7 +26,8 @@
           </v-list-item>
         </v-list-item-group>
       </v-list>
-    </v-card>
+    </v-card> -->
+
     <v-dialog v-model="dialog" persistent max-width="360">
       <v-card>
         <v-card-title class="text-h5">溫馨提醒</v-card-title>
@@ -54,15 +40,6 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-
-    <v-card class="mx-auto mt-5">
-      <v-card-title> Hi 王小明 </v-card-title>
-      <v-card-text>
-        <p>2021年07月28日</p>
-        <p>星期三</p>
-        <p>09:30</p>
-      </v-card-text>
-    </v-card>
   </v-container>
 </template>
 
@@ -76,8 +53,8 @@ export default {
         { text: "我要打卡", icon: "mdi-account-check" },
         { text: "打卡紀錄", icon: "mdi-history" },
         { text: "補登申請", icon: "mdi-book-plus-multiple-outline" },
-        { text: "補登審核", icon: "mdi-account-multiple-check-outline" },
-        { text: "人事查詢", icon: "mdi-key-link" },
+        // { text: "補登審核", icon: "mdi-account-multiple-check-outline" },
+        // { text: "人事查詢", icon: "mdi-key-link" },
       ],
     };
   },
