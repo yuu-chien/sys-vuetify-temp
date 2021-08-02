bc990e0ce9a781e35dcfa3429ec77ee45a895cfc
chien.yu <sarsh40320@gmail.com>
2021-08-02 19:00:11 +0800

    [ Layout ] Move dialog to home

--- src/views/layout/Home.vue
+++ src/views/layout/Home.vue
@@ -1,27 +1,43 @@
 <template>
-  <v-card>
-    <v-card-title>本月未打卡紀錄如下，請申請補登</v-card-title>
-    <v-list>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>YYYY 年 MM 月 DD 日</v-list-item-title>
-          <v-list-item-subtitle class="pt-4"></v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title>YYYY 年 MM 月 DD 日</v-list-item-title>
-          <v-list-item-subtitle class="pt-4"></v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-card>
+  <div>
+    <v-card class="rounded-lg">
+      <v-card-title>本月未打卡紀錄如下，請申請補登</v-card-title>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>YYYY 年 MM 月 DD 日</v-list-item-title>
+            <v-list-item-subtitle class="pt-4"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>YYYY 年 MM 月 DD 日</v-list-item-title>
+            <v-list-item-subtitle class="pt-4"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-card>
+    <v-dialog v-model="dialog" persistent max-width="360">
+      <v-card>
+        <v-card-title class="text-h5">溫馨提醒</v-card-title>
+        <v-card-text>您有 N 筆補登審核未處理</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="dialog = false"
+            >知道了</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      dialog: true,
+    };
   },
 };
 </script>
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container>
+  <v-container class="pa-4">
     <router-view></router-view>
-    <v-bottom-navigation fixed shift color="orange" class="p-0" height="auto">
+    <v-bottom-navigation app shift color="orange" height="auto" class="pa-0">
       <v-btn
         v-for="(item, i) in items"
         :key="i"
@@ -13,34 +13,6 @@
         <v-icon>{{ item.icon }}</v-icon>
       </v-btn>
     </v-bottom-navigation>
-
-    <!-- <v-card max-width="360">
-      <v-list shaped>
-        <v-list-item-group v-model="selectedItem" color="orange">
-          <v-list-item v-for="(item, i) in items" :key="i">
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-card> -->
-
-    <v-dialog v-model="dialog" persistent max-width="360">
-      <v-card>
-        <v-card-title class="text-h5">溫馨提醒</v-card-title>
-        <v-card-text>您有 N 筆補登審核未處理</v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="dialog = false"
-            >知道了</v-btn
-          >
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-container>
 </template>
 
@@ -48,7 +20,6 @@
 export default {
   data() {
     return {
-      dialog: false,
       selectedItem: 1,
       items: [
         { text: "我要打卡", icon: "mdi-account-check", link: "clock-in" },
