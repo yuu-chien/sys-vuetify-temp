590f487e139d7a3391b973cd2d454e5dd7e84c16
yuu-chien <55533267+yuu-chien@users.noreply.github.com>
2021-08-05 23:39:08 +0800

    [ Layout ] Change menu from bottom to left

--- src/router/index.js
+++ src/router/index.js
@@ -5,46 +5,46 @@ import Layout from '../views/layout/Layout.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '',
-    name: 'layout',
-    component: Layout,
-    children: [
-      {
-        path: '/',
-        component: () => import('../views/layout/Home.vue'),
-      },
-      {
-        path: 'clock-in',
-        component: () => import('../views/layout/Clockin.vue'),
-      },
-      {
-        path: 'mending',
-        component: () => import('../views/layout/Mending.vue'),
-      },
-      {
-        path: 'records',
-        component: () => import('../views/layout/Records.vue'),
-      }
-    ]
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  }
+	{
+		path: '',
+		name: 'layout',
+		component: Layout,
+		children: [
+			{
+				path: '/',
+				component: () => import('../views/layout/Home.vue'),
+			},
+			{
+				path: 'punch',
+				component: () => import('../views/layout/Punch.vue'),
+			},
+			{
+				path: 'application',
+				component: () => import('../views/layout/Application.vue'),
+			},
+			{
+				path: 'records',
+				component: () => import('../views/layout/Records.vue'),
+			}
+		]
+	},
+	{
+		path: '/about',
+		name: 'About',
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		component: () => import('../views/Login.vue')
+	}
 ]
 
 const router = new VueRouter({
-  routes
+	routes
 })
 
 export default router
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,70 +1,85 @@
 <template>
-    <v-container class="pa-4">
-        <router-view></router-view>
-        <v-bottom-navigation
-            app
-            shift
-            :value="currentColor"
-            color="orange"
-            class="pa-0"
+  <v-container>
+    <!-- <v-bottom-navigation
+      app
+      shift
+      :value="currentColor"
+      color="orange"
+      class="pa-0"
+    >
+      <v-btn
+        v-for="(item, i) in items"
+        :key="i"
+        :to="{ path: item.link }"
+        x-large
+        color="white"
+      >
+        <span class="mt-1">{{ item.text }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation> -->
+    <v-navigation-drawer app permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Jason E. Hibbitts
+          </v-list-item-title>
+          <v-list-item-subtitle> PD </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item
+          v-for="item in items"
+          :key="item.text"
+          link
+          :to="{ path: item.link }"
         >
-            <v-btn
-                v-for="(item, i) in items"
-                :key="i"
-                :to="{ path: item.link }"
-                x-large
-                color="white"
-            >
-                <span class="mt-1">{{ item.text }}</span>
-                <v-icon>{{ item.icon }}</v-icon>
-            </v-btn>
-        </v-bottom-navigation>
-    </v-container>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <router-view></router-view>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            computed: {
-                currentColor() {
-                    let result = ''
-                    switch (this.$router.history.current.name) {
-                        case 'hr':
-                            result = 1
-                            break
-                        case 'records':
-                            result = 2
-                            break
-                        case 'mending':
-                            result = 3
-                            break
-                        case 'logout':
-                            result = 4
-                            break
-                    }
-                    return result
-                },
-            },
-            items: [
-                { text: '首頁', icon: 'mdi-home-variant-outline', link: '/' },
-                {
-                    text: '人事查詢',
-                    icon: 'mdi-newspaper-variant-multiple-outline',
-                    link: ' ',
-                },
-                {
-                    text: '補登審核',
-                    icon: 'mdi-account-multiple-plus-outline',
-                    link: 'examine',
-                },
-                {
-                    text: '登出',
-                    icon: 'mdi-login-variant',
-                    link: 'logout',
-                },
-            ],
-        }
-    },
-}
+  data() {
+    return {
+      computed: {},
+      items: [
+        { text: "Home", icon: "mdi-home-variant-outline", link: "/" },
+        {
+          text: "Punch in / out",
+          icon: "mdi-account-check",
+          link: "punch",
+        },
+        { text: "Records", icon: "mdi-history", link: "records" },
+        {
+          text: "Application",
+          icon: "mdi-newspaper-variant-multiple-outline",
+          link: "application",
+        },
+        // {
+        //   text: "Setting",
+        //   icon: "mdi-account-cog-outline",
+        //   link: " ",
+        // },
+        {
+          text: "",
+          icon: "mdi-login-variant",
+          link: "login",
+        },
+      ],
+    };
+  },
+};
 </script>
