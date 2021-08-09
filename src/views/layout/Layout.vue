22550893b63b16078125a9f873272ba2af4ce1a8
chien.yu <sarsh40320@gmail.com>
2021-08-09 18:51:17 +0800

    [ Layout ] Collapse menu

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container>
-    <!-- <v-bottom-navigation
+    <v-container>
+        <!-- <v-bottom-navigation
       app
       shift
       :value="currentColor"
@@ -18,68 +18,70 @@
         <v-icon>{{ item.icon }}</v-icon>
       </v-btn>
     </v-bottom-navigation> -->
-    <v-navigation-drawer app permanent>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Jason E. Hibbitts
-          </v-list-item-title>
-          <v-list-item-subtitle> PD </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+        <div>
+            <v-icon class="my-2" @click.stop="drawer = !drawer"
+                >mdi-backburger</v-icon
+            >
+        </div>
+        <v-navigation-drawer app v-model="drawer" absolute temporary>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="d-flex justify-space-between">
+                        <div class="text-h6">Jason E. Hibbitts</div>
+                        <!-- <v-icon>mdi-menu</v-icon> -->
+                    </v-list-item-title>
+                    <v-list-item-subtitle> PD </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
 
-      <v-divider></v-divider>
+            <v-divider></v-divider>
 
-      <v-list dense nav>
-        <v-list-item
-          v-for="item in items"
-          :key="item.text"
-          link
-          :to="{ path: item.link }"
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <router-view></router-view>
-  </v-container>
+            <v-list dense nav>
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.text"
+                    link
+                    :to="{ path: item.link }"
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <router-view></router-view>
+    </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      computed: {},
-      items: [
-        { text: "Home", icon: "mdi-home-variant-outline", link: "/" },
-        {
-          text: "Punch in / out",
-          icon: "mdi-account-check",
-          link: "punch",
-        },
-        { text: "Records", icon: "mdi-history", link: "records" },
-        {
-          text: "Application",
-          icon: "mdi-newspaper-variant-multiple-outline",
-          link: "application",
-        },
-        // {
-        //   text: "Setting",
-        //   icon: "mdi-account-cog-outline",
-        //   link: " ",
-        // },
-        {
-          text: "",
-          icon: "mdi-login-variant",
-          link: "login",
-        },
-      ],
-    };
-  },
-};
+    data() {
+        return {
+            computed: {},
+            drawer: null,
+            items: [
+                { text: 'Home', icon: 'mdi-home-variant-outline', link: '/' },
+                {
+                    text: 'Punch in / out',
+                    icon: 'mdi-account-check',
+                    link: 'punch',
+                },
+                { text: 'Records', icon: 'mdi-history', link: 'records' },
+                {
+                    text: 'Application',
+                    icon: 'mdi-newspaper-variant-multiple-outline',
+                    link: 'application',
+                },
+                {
+                    text: '',
+                    icon: 'mdi-login-variant',
+                    link: 'login',
+                },
+            ],
+        }
+    },
+}
 </script>
