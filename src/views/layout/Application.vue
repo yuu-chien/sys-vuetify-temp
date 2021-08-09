b25f1074ada82b3357ec2f70bcb8aef8356b69b0
chien.yu <sarsh40320@gmail.com>
2021-08-09 18:51:47 +0800

    [ All ] Change card's min-width

--- src/views/layout/Application.vue
+++ src/views/layout/Application.vue
@@ -1,119 +1,134 @@
 <template>
-  <v-card outlined min-width="360" class="mx-auto rounded-lg">
-    <v-card-title class="orange--text font-weight-bold">
-      <v-icon color="orange" class="mr-3">
-        mdi-newspaper-variant-multiple-outline
-      </v-icon>
-      <div>Application</div>
-    </v-card-title>
-    <v-list class="py-0">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="mb-0 font-weight-bold"
-            >Types</v-list-item-title
-          >
-          <v-radio-group row dense hide-details="auto" class="mt-1">
-            <v-radio label="Punch-in" value="" color="orange"></v-radio>
-            <v-radio label="Punch-out" value="" color="orange"></v-radio>
-          </v-radio-group>
-        </v-list-item-content>
-      </v-list-item>
+    <v-card outlined min-width="320" class="mx-auto rounded-lg">
+        <v-card-title class="orange--text font-weight-bold">
+            <v-icon color="orange" class="mr-3">
+                mdi-newspaper-variant-multiple-outline
+            </v-icon>
+            <div>Application</div>
+        </v-card-title>
+        <v-list class="py-0">
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="mb-0 font-weight-bold"
+                        >Types</v-list-item-title
+                    >
+                    <v-radio-group row dense hide-details="auto" class="mt-1">
+                        <v-radio
+                            label="Punch-in"
+                            value=""
+                            color="orange"
+                        ></v-radio>
+                        <v-radio
+                            label="Punch-out"
+                            value=""
+                            color="orange"
+                        ></v-radio>
+                    </v-radio-group>
+                </v-list-item-content>
+            </v-list-item>
 
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="mb-0 font-weight-bold"
-            >Date</v-list-item-title
-          >
-          <div>
-            <v-menu
-              :close-on-content-click="false"
-              :nudge-right="40"
-              transition="scale-transition"
-              offset-y
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-text-field
-                  readonly
-                  hide-details="auto"
-                  v-model="date"
-                  label=""
-                  v-bind="attrs"
-                  v-on="on"
-                  color="orange"
-                  class="mt-0"
-                ></v-text-field>
-              </template>
-              <v-date-picker color="orange" v-model="date"></v-date-picker>
-            </v-menu>
-          </div>
-        </v-list-item-content>
-      </v-list-item>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="mb-0 font-weight-bold"
+                        >Date</v-list-item-title
+                    >
+                    <div>
+                        <v-menu
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            transition="scale-transition"
+                            offset-y
+                        >
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-text-field
+                                    readonly
+                                    hide-details="auto"
+                                    v-model="date"
+                                    label=""
+                                    v-bind="attrs"
+                                    v-on="on"
+                                    color="orange"
+                                    class="mt-0"
+                                ></v-text-field>
+                            </template>
+                            <v-date-picker
+                                color="orange"
+                                v-model="date"
+                            ></v-date-picker>
+                        </v-menu>
+                    </div>
+                </v-list-item-content>
+            </v-list-item>
 
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="mb-0 font-weight-bold"
-            >Cause</v-list-item-title
-          >
-          <v-text-field
-            :rules="rules"
-            counter="20"
-            hide-details="auto"
-            hint=""
-            label=""
-            color="orange"
-            class="mt-0"
-          ></v-text-field>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-    <v-card-actions class="flex-column">
-      <v-dialog transition="dialog-top-transition" max-width="360">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            plain
-            text
-            depressed
-            :to="{ path: '/' }"
-            color="grey darken-1"
-            class="px-0"
-            >Cancel</v-btn
-          >
-          <v-btn
-            plain
-            text
-            depressed
-            v-on="on"
-            v-bind="attrs"
-            color="orange"
-            class="px-0"
-            >Send</v-btn
-          >
-        </template>
-        <template v-slot:default="dialog">
-          <v-card>
-            <v-card-title class="text-h5 green--text font-weight-bold"
-              >Apply Successful !</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="orange" text @click="dialog.value = false"
-                >OK</v-btn
-              >
-            </v-card-actions>
-          </v-card>
-        </template>
-      </v-dialog>
-    </v-card-actions>
-  </v-card>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="mb-0 font-weight-bold"
+                        >Cause</v-list-item-title
+                    >
+                    <v-text-field
+                        :rules="rules"
+                        counter="20"
+                        hide-details="auto"
+                        hint=""
+                        label=""
+                        color="orange"
+                        class="mt-0"
+                    ></v-text-field>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+        <v-card-actions class="flex-column">
+            <v-dialog transition="dialog-top-transition" max-width="360">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        plain
+                        text
+                        depressed
+                        :to="{ path: '/' }"
+                        color="grey darken-1"
+                        class="px-0"
+                        >Cancel</v-btn
+                    >
+                    <v-btn
+                        plain
+                        text
+                        depressed
+                        v-on="on"
+                        v-bind="attrs"
+                        color="orange"
+                        class="px-0"
+                        >Send</v-btn
+                    >
+                </template>
+                <template v-slot:default="dialog">
+                    <v-card>
+                        <v-card-title
+                            class="text-h5 green--text font-weight-bold"
+                            >Apply Successful !</v-card-title
+                        >
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                color="orange"
+                                text
+                                @click="dialog.value = false"
+                                >OK</v-btn
+                            >
+                        </v-card-actions>
+                    </v-card>
+                </template>
+            </v-dialog>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-      .toISOString()
-      .substr(0, 10),
-    rules: [(v) => (v && v.length <= 20) || "Max 20 characters"],
-  }),
-};
+    data: () => ({
+        date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+            .toISOString()
+            .substr(0, 10),
+        rules: [(v) => (v && v.length <= 20) || 'Max 20 characters'],
+    }),
+}
 </script>
--- src/views/layout/Records.vue
+++ src/views/layout/Records.vue
@@ -1,81 +1,84 @@
 <template>
-  <div max-width="360">
-    <div class="d-flex justify-space-between">
-      <v-btn plain large class="px-0">
-        <v-icon left> mdi-chevron-left </v-icon>
-        <span>Previous</span>
-      </v-btn>
-      <v-btn plain large class="px-0">
-        <span>Next</span>
-        <v-icon right> mdi-chevron-right </v-icon>
-      </v-btn>
+    <div max-width="360">
+        <div class="d-flex justify-space-between">
+            <v-btn plain large class="px-0">
+                <v-icon left> mdi-chevron-left </v-icon>
+                <span>Previous</span>
+            </v-btn>
+            <v-btn plain large class="px-0">
+                <span>Next</span>
+                <v-icon right> mdi-chevron-right </v-icon>
+            </v-btn>
+        </div>
+        <v-expansion-panels>
+            <v-expansion-panel v-for="(item, i) in records" :key="i">
+                <v-expansion-panel-header>
+                    <h3>2021 / 8 / {{ i + 2 }}（{{ item }}）</h3>
+                    <v-icon color="red lighten-1" v-if="i == 1"
+                        >mdi-information</v-icon
+                    >
+                    <v-icon color="red lighten-1" v-else></v-icon>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    <v-list class="py-0">
+                        <v-list-item class="px-0">
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title class="mb-0"
+                                    >Punch in - 09:30</v-list-item-title
+                                >
+                                <p class="pt-1 grey--text body-2">
+                                    Lorem ipsum, dolor sit amet consectetur
+                                    adipisicing elit. Doloremque, in architecto
+                                    officia libero ullam cumque distinctio
+                                    cupiditate quibusdam et temporibus?
+                                </p>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item class="px-0">
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title class="mb-0" v-if="i == 1"
+                                    >Punch out - no data
+                                    <v-btn
+                                        small
+                                        rounded
+                                        outlined
+                                        :to="{ path: 'mending' }"
+                                        color="orange"
+                                        class="ml-2"
+                                    >
+                                        Apply
+                                    </v-btn></v-list-item-title
+                                >
+                                <v-list-item-title class="mb-0" v-else
+                                    >Punch out - 18:30</v-list-item-title
+                                >
+                                <p class="pt-1 grey--text body-2">No notes</p>
+                            </v-list-item-content>
+                        </v-list-item>
+                        <v-list-item class="px-0">
+                            <v-list-item-content class="py-0">
+                                <v-list-item-title class="mb-0" v-if="i == 1"
+                                    >hours -</v-list-item-title
+                                >
+                                <v-list-item-title class="mb-0" v-else
+                                    >hours - 8</v-list-item-title
+                                >
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
     </div>
-    <v-expansion-panels>
-      <v-expansion-panel v-for="(item, i) in records" :key="i">
-        <v-expansion-panel-header>
-          <h3>2021 / 8 / {{ i + 2 }} 日（{{ item }}）</h3>
-          <v-icon color="red lighten-1" v-if="i == 1">mdi-information</v-icon>
-          <v-icon color="red lighten-1" v-else></v-icon>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-list class="py-0">
-            <v-list-item class="px-0">
-              <v-list-item-content class="py-0">
-                <v-list-item-title class="mb-0"
-                  >Punch in - 09:30</v-list-item-title
-                >
-                <p class="pt-1 grey--text body-2">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Doloremque, in architecto officia libero ullam cumque
-                  distinctio cupiditate quibusdam et temporibus?
-                </p>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item class="px-0">
-              <v-list-item-content class="py-0">
-                <v-list-item-title class="mb-0" v-if="i == 1"
-                  >Punch out - no data
-                  <v-btn
-                    small
-                    rounded
-                    outlined
-                    :to="{ path: 'mending' }"
-                    color="orange"
-                    class="ml-2"
-                  >
-                    Apply
-                  </v-btn></v-list-item-title
-                >
-                <v-list-item-title class="mb-0" v-else
-                  >Punch out - 18:30</v-list-item-title
-                >
-                <p class="pt-1 grey--text body-2">No notes</p>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item class="px-0">
-              <v-list-item-content class="py-0">
-                <v-list-item-title class="mb-0" v-if="i == 1"
-                  >hours -</v-list-item-title
-                >
-                <v-list-item-title class="mb-0" v-else
-                  >hours - 8</v-list-item-title
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </div>
 </template>
 
 
 <script>
 export default {
-  data() {
-    return {
-      records: ["Mon", "Tue", "Wen", "Thu", "Fri"],
-    };
-  },
-};
+    data() {
+        return {
+            records: ['Mon', 'Tue', 'Wen', 'Thu', 'Fri'],
+        }
+    },
+}
 </script>
